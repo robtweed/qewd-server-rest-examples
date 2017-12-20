2a6aeadf14fa433e92153405a1e7d6b394642dc1
robtweed <rtweed@mgateway.com>
2017-12-20 16:57:12 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# qewd-server-example: A basic set of REST APIs to test on your Docker qewd-server
+ 
+Rob Tweed <rtweed@mgateway.com>  
+20 December 2017, M/Gateway Developments Ltd [http://www.mgateway.com](http://www.mgateway.com)  
+
+Twitter: @rtweed
+
+Google Group for discussions, support, advice etc: [http://groups.google.co.uk/group/enterprise-web-developer-community](http://groups.google.co.uk/group/enterprise-web-developer-community)
+
+## Installing
+
+Clone this repository onto the machine hosting your Docker *qewd-server* Container
+
+
+## License
+
+ Copyright (c) 2017 M/Gateway Developments Ltd,                           
+ Reigate, Surrey UK.                                                      
+ All rights reserved.                                                     
+                                                                           
+  http://www.mgateway.com                                                  
+  Email: rtweed@mgateway.com                                               
+                                                                           
+                                                                           
+  Licensed under the Apache License, Version 2.0 (the "License");          
+  you may not use this file except in compliance with the License.         
+  You may obtain a copy of the License at                                  
+                                                                           
+      http://www.apache.org/licenses/LICENSE-2.0                           
+                                                                           
+  Unless required by applicable law or agreed to in writing, software      
+  distributed under the License is distributed on an "AS IS" BASIS,        
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+  See the License for the specific language governing permissions and      
+   limitations under the License.      
---
+++ modules/example-db/handlers/test_db_post.js
@@ -0,0 +1,37 @@
+/*
+
+ ----------------------------------------------------------------------------
+ | qewd-server-rest-examples:                                               |
+ |     A basic set of REST APIs to test on your Docker qewd-server          |
+ |                                                                          |
+ | Copyright (c) 2017 M/Gateway Developments Ltd,                           |
+ | Reigate, Surrey UK.                                                      |
+ | All rights reserved.                                                     |
+ |                                                                          |
+ | http://www.mgateway.com                                                  |
+ | Email: rtweed@mgateway.com                                               |
+ |                                                                          |
+ |                                                                          |
+ | Licensed under the Apache License, Version 2.0 (the "License");          |
+ | you may not use this file except in compliance with the License.         |
+ | You may obtain a copy of the License at                                  |
+ |                                                                          |
+ |     http://www.apache.org/licenses/LICENSE-2.0                           |
+ |                                                                          |
+ | Unless required by applicable law or agreed to in writing, software      |
+ | distributed under the License is distributed on an "AS IS" BASIS,        |
+ | WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. |
+ | See the License for the specific language governing permissions and      |
+ |  limitations under the License.                                          |
+ ----------------------------------------------------------------------------
+
+  20 December 2017
+
+*/
+
+
+var db_post = require('../db/post');
+
+module.exports = function(args, finished) {
+  finished(db_post.call(this, 'TestDocuments', args.documentName, args.req.body));
+};
